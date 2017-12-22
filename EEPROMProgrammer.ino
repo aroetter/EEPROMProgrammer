fc157a9c8fdf4aed7f6c21fbbcb7aef11300fdf5
Alex Roetter <aroetter@users.noreply.github.com>
2017-12-21 16:21:08 -0800

    extend to 24 bits of controllines also skeleton microcode for the 'load prog from EEPROM to RAM' neither is tested yet!

--- EEPROMProgrammer.ino
+++ EEPROMProgrammer.ino
@@ -1,4 +1,4 @@
-// define which pins are what for IO
+// define which arduino pins are used for what
 #define SHIFT_DATA    2
 #define SHIFT_CLK     3
 #define SHIFT_LATCH   4
@@ -89,11 +89,11 @@ void printContents() {
 // [0000...0255]: ones place digit for unsigned decimal representation
 // [0256...0511]: tens place digit     ""              ""
 // [0512...0767]: hundreds place digit ""              ""
-// [0768...1023]: signs place digit (always blank for unsigned numbers)
+// [0768...1023]: signs place digit    ""              "" (always blank for unsigned numbers)
 // [1024...1279]: ones place digit for signed decimal representation
 // [1280...1535]: tens place digit     ""              ""
 // [1536...1791]: hundreds place digit ""              "" 
-// [1792...2047]: signs place digit (either a negative sign or blank)
+// [1792...2047]: signs place digit    ""              "" (either a negative sign or blank)
 void write7SegmentDecimalDisplayEEPROM() {
   // represent decimal digits 0 ->10 (the 8 bits for a screen display)
   byte digits[] = { 0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b};
@@ -139,40 +139,44 @@ void eraseEEPROM() {
   Serial.println("done.");
 }
 
-// Control Line Bits. There are 16 bits that can be control lines
-// Left EEPROM control bits
-static uint16_t HALT = 0x8000; // Clock - Stop the clock
-static uint16_t   MI = 0x4000; // Memory Address Register - Input from Bus
-static uint16_t   RI = 0x2000; // RAM - Input from Bus
-static uint16_t   RO = 0x1000; // RAM - Output to Bus
-static uint16_t   IO = 0x0800; // Instruction Register - Output to Bus
-static uint16_t   II = 0x0400; // Instruction Register - Input from Bus
-static uint16_t   AI = 0x0200; // A Register - Input from Bus
-static uint16_t   AO = 0x0100; // A Register - Output to Bus
+// Control Line Bits. Top byte here is unused, leaving 24 bits for control lines.
+// Left EEPROM control bits (2nd most significant byte)
+static uint32_t HALT = 0x00800000; // Clock - Stop the clock
+static uint32_t   MI = 0x00400000; // Memory Address Register - Input from Bus
+static uint32_t   RI = 0x00200000; // RAM - Input from Bus
+static uint32_t   RO = 0x00100000; // RAM - Output to Bus
+static uint32_t   IO = 0x00080000; // Instruction Register - Output to Bus
+static uint32_t   II = 0x00040000; // Instruction Register - Input from Bus
+static uint32_t   AI = 0x00020000; // A Register - Input from Bus
+static uint32_t   AO = 0x00010000; // A Register - Output to Bus
 
-// Right EEPROM control bits
-static uint16_t   SO = 0x0080; // ALU - Output Sum to Bus
-static uint16_t   SU = 0x0040; // ALU - Subtract mode (vs. Add otherwise)
-static uint16_t   BI = 0x0020; // B Register - Input from Bus
-static uint16_t   OI = 0x0010; // Output (LCD) Register - Input from Bus
+// Center EEPROM control bits (3rd most significant byte)
+static uint32_t   SO = 0x00008000; // ALU - Output Sum to Bus
+static uint32_t   SU = 0x00004000; // ALU - Subtract mode (vs. Add otherwise)
+static uint32_t   BI = 0x00002000; // B Register - Input from Bus
+static uint32_t   OI = 0x00001000; // Output (LCD) Register - Input from Bus
+static uint32_t   CE = 0x00000800; // Program Counter - Counter Enable: PC will increment on each clock
+static uint32_t   CO = 0x00000400; // Program Counter - Output to Bus
+static uint32_t    J = 0x00000200; // Program Counter - Jump: Input from Bus
+static uint32_t   HO = 0x00000100; // Stored Program ("Hard Drive") EEPROM: Output to Bus
 
-static uint16_t   CE = 0x0008; // Program Counter - Counter Enable: PC will increment on each clock
-static uint16_t   CO = 0x0004; // Program Counter - Output to Bus
-static uint16_t    J = 0x0002; // Program Counter - Jump: Input from Bus
-static uint16_t   HO = 0x0001; // Stored Program ("Hard Drive") EEPROM: Output to Bus
+// Right EEPROM control bits (least significant byte)
+static uint32_t   X0 = 0x00000080; // Input Register X - Write to Bus
+// down to             0x00000001; // last control bit
 
 // This is changable by moving the reset wire on the 3 bit counter on the control
 // logic breadboard. This only exists here as a sanity check to enforce consistency
-// with other datastructures below.
+// with other data structures below.
 #define NUM_MICROCODE_PER_OPCODE 5
 
 // Every instruction starts with a fetch, so we program that here.
 // Way to read this is:
 // On 1st clock cycle, a fetch sets the CO & MI conrol lines
 // On 2nd clock cycle, a fetch sets the RO, II, & CE control lines
-static uint16_t FETCH_MICROCODE[] = {CO | MI, RO | II | CE };
+static uint32_t FETCH_MICROCODE[] = {CO | MI, RO | II | CE };
+static int FETCH_MICROCODE_LEN = sizeof(FETCH_MICROCODE) / FETCH_MICROCODE[0];
 
-// must be == (NUM_MICROCODE_PER_OPCODE - len(FETCH_MICROCODE).
+// must be == (NUM_MICROCODE_PER_OPCODE - FETCH_MICROCODE_LEN).
 // will fail initialization checks if not
 #define NUM_CUSTOM_MICROCODE_PER_OPCODE 3
 
@@ -180,8 +184,9 @@ static uint16_t FETCH_MICROCODE[] = {CO | MI, RO | II | CE };
 #define NUM_OPCODES 16
 
 // Define the symbolic assembler instruction name to binary opcode mapping here.
-// We use these to write programs. These must be consecutive and in-order in
-// the OpCodeDefT array below (this is checked at init time).
+// We use these to write programs. These must be consecutive integers starting
+// at zero, and in the same order as the OpCodeDefT OPCODE[] array below
+// (this is checked at init time).
 enum OpCodeName {
   NOP = 0b0000,
   LDA = 0b0001, // Load A (write from RAM into A)
@@ -207,7 +212,7 @@ enum OpCodeName {
 typedef struct OpCodeDefT {
   const char* name; // TODO: delete this field I think. TODO
   OpCodeName opcode;
-  uint16_t microcode[NUM_CUSTOM_MICROCODE_PER_OPCODE];
+  uint32_t microcode[NUM_CUSTOM_MICROCODE_PER_OPCODE];
 } OpCodeDefT;
 
 
@@ -246,19 +251,15 @@ void doCommonInit() {
 
   // Sanity check the static microcode definitions
   // Do we have the right number of microcode definitions?
-  if (NUM_OPCODES * sizeof(OpCodeDefT) != sizeof(OPCODE)) {
-    char buf[100];
-    snprintf(buf, 100, "Must have exactly %d microcodes defined. Instead saw %d. Aborting. Terminating...",
-      NUM_OPCODES, sizeof(OPCODE) / sizeof(OpCodeDefT));
-    Serial.println(buf);
+  if (sizeof(OPCODE) / sizeof(OPCODE[0]) != NUM_OPCODES) {
+    Serial.println("Wrong number of opcodes defined!");
     Serial.flush();
     abort();
   }
 
   // Does each opcode have the right number of microcode steps?
-  int fetch_microcode_len = sizeof(FETCH_MICROCODE) / sizeof(uint16_t);
-  int per_opcode_microcode_len = sizeof(OPCODE[0].microcode) / sizeof(uint16_t);
-  if ((per_opcode_microcode_len + fetch_microcode_len) != NUM_MICROCODE_PER_OPCODE) {
+  int per_opcode_microcode_len = sizeof(OPCODE[0].microcode) / sizeof(OPCODE[0].microcode[0]);
+  if ((per_opcode_microcode_len + FETCH_MICROCODE_LEN) != NUM_MICROCODE_PER_OPCODE) {
     char buf[100];
     snprintf(buf, 100, "Each opcode must be = %d microcodes. Fix static data structures. Terminating...",
       NUM_MICROCODE_PER_OPCODE);
@@ -270,53 +271,107 @@ void doCommonInit() {
   // make sure each opcode is defined in order with a consecutive binary value
   for (int i = 0; i < NUM_OPCODES; ++i) {
     if (OPCODE[i].opcode != i) {
-      Serial.println("Each opcode must have a consecutive, increasing, binary value!");
+      Serial.println("Each opcode must have a consecutive and increasing, binary value!");
       Serial.flush();
       abort();
     }
   }
 }
 
+// Write the given 3-byte data to three different EEPROMs at the specified address
+// modify the addresses bits a8 & a7 to specify which of the 3 EEPROMs.
+// 2nd most-sig byte goes to LEFT_EEPROM, next byte goes to CENTER_EEPROM,
+// least sig byte goes to RIGHT_EEPROM
+void write24BitControlWordToEEPROMs(uint16_t addr, uint32_t data) {
+  const uint16_t LEFT_EEPROM =   0x000;
+  const uint16_t CENTER_EEPROM = 0x080;  
+  const uint16_t RIGHT_EEPROM =  0x100;
 
-// Set up microcode for out EEPROM, which is addressable via 11 address lines [a10...a0]
+  // write data for the all 3 EEPROMs. Hard wiring (per physical EEPROM position on the breadboard)
+  // will determine which one is read
+  writeEEPROM(addr | LEFT_EEPROM, data >> 16);
+  writeEEPROM(addr | CENTER_EEPROM, data >> 8);
+  writeEEPROM(addr | RIGHT_EEPROM, data);
+}
+
+
+// Write the Microcode EEPROMs. EEPROMs are addressable via 11 address lines [a10...a0]
+// Addressing for the microcode EEPROMS is done as follows:
 //
-// Addressing for the microcode is done as follows:
-// a10...a8: unused, always 0.
-// a7: which EEPROM. 0 for left, 1 for right EEPROM
-// a6....a3: 4 bits to represent the opcode
-// a2....a0: 3 bits to represent the microcode step we are on (only 0-4 used though we could extend to 0-7)
+// a10: unused, always 0 (1 means we are on the stored EEPROM, see below).
+// a09: MC type. 0 for regular microcode to execute programs from RAM,
+//               1 for the microcode to load a program from the "hard-drive (stored assembly program)" EEPROM
+// a8-a7: which EEPROM. 00 for left, 01 for center, 10 for right EEPROM
+//
+// Remaining bits depend on the MC type.
+//
+// When MC type == 0: (Running a program from RAM)
+//   a6-a5-a4-a3: 4 bits to represent the opcode
+//   a2....a0: 3 bits to represent the microcode step we are on (only 0-4 used per NUM_MICROCODE_PER_OPCODE)
+// When MC type == 1: Running the "load pre-canned program from 'hard-drive' EEPROM"
+//   a6: unused, always 0
+//   a5-a4-a3-a2-a1-a0: the microcode step to run of the "load program from another EEPROM into RAM" program
+//                      can be up to 64 steps (2^6)
+//     
 void writeMicroCodeEEPROM() {  
-  const int fetch_microcode_len = sizeof(FETCH_MICROCODE) / sizeof(uint16_t);
-  
   // Iterate over every microcode step w/in that opcode
   // Set the fetch instructions, then the custom microcode for each one
-  for(uint8_t i = 0; i < NUM_OPCODES; ++i) {
+  for(uint8_t opcode = 0; opcode < NUM_OPCODES; ++opcode) {
     char buf[100];
-    OpCodeDefT mc = OPCODE[i];
+    OpCodeDefT mc = OPCODE[opcode];
     char binaryStr[5];
-    convert4BitIntToBinaryString(binaryStr, i);
+    convert4BitIntToBinaryString(binaryStr, opcode);
     snprintf(buf, 100, "Programming opcode %s (binary opcode = %s)", mc.name, binaryStr);
     Serial.println(buf);
     for(uint8_t step = 0; step < NUM_MICROCODE_PER_OPCODE; ++step) {
-      // generate a 16 bit address of form 00000000 woooosss
-      // w is which EEPROM (0 for left, 1 for right).
-      // where oooo is the 4 bit opcode, and sss is the 3 bit step
-      // top 5 bits are unused, EEPROM uses 11 lower bits for address only
-      uint16_t addr = (i << 3) | step; 
-      // write either the fetch steps, or the custom control logic for the opcode
-      uint16_t data;
-      if (step < fetch_microcode_len) {
+      // generate the lower 7 bits of the address: for opcode and microcode step count
+      uint16_t addr = (opcode << 3) | step;
+       
+      // for the current step: write either the fetch microcode instruction, or the opcode specific microcode
+      uint32_t data;
+      if (step < FETCH_MICROCODE_LEN) {
         data = FETCH_MICROCODE[step];
       } else {
-        data = mc.microcode[step - fetch_microcode_len];
+        data = mc.microcode[step - FETCH_MICROCODE_LEN];
       }
-
-      // now write the data
-      writeEEPROM(addr, data >> 8 /* write upper 8 bits */);
-      addr |= 0x0080; // set the bit called 'w' above
-      writeEEPROM(addr, data /* write lower 8 bits */);
+      write24BitControlWordToEEPROMs(addr, data);
     }
   }
+
+  // Now write out the microcode instructions for the "Load program from another EEPROM into RAM"
+  Serial.println("Programming microcode to load stored program from another EEPROM into RAM.");
+  // TODO: hardcode program in a static array
+  // TODO: check compiler enforces this as 64.
+  static int LOAD_PROG_MICROCODE_LEN = 64;
+  // TODO: write this program, basically 2 microcodeinstructions repeated 16 times, then cleanup.
+  static uint32_t LOAD_PROG_MICROCODE[] = {
+    CO|MI, HO|RI|CE, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    HALT, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+  };
+  
+  if ((sizeof(LOAD_PROG_MICROCODE) / sizeof(LOAD_PROG_MICROCODE[0])) != LOAD_PROG_MICROCODE_LEN) {
+      Serial.println("Wrong sized 'Load Program from EEPROM' microcode definition!");
+      Serial.flush();
+      abort();
+  }
+  
+  uint16_t addr = 0x200; // set a9.
+  for (byte i = 0; i < LOAD_PROG_MICROCODE_LEN; ++i) {
+    write24BitControlWordToEEPROMs(addr | i, LOAD_PROG_MICROCODE[i]);
+  }
+}
+
+// TODO: implement this
+void writeStoredProgramEEPROM() {
+  // Note addressing here is mutually exclusive with microcodeEEPROM, meaning we can load a single
+  // physical chip with both sets of data
+  // addressing for 11 address bits is
+  // a10: 1 (meaning we are in the stored program EEPROM, as opposed to microcode EEPROMs).
+  // a9-a8-a7-a6: unused, always 0.
+  // a6-a5-a4: with program are we loading into RAM (0-7)
+  // a3-a2-a1-a0: which assembly language instruction are we at for this program (0-15)
 }
 
 /* Arduino runs this function once after loading the Nano, or after pressing the HW reset button.
